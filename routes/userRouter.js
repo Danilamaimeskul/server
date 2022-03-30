3e420ab48cc3b4dffb74b5b7eb5780d87051f008
Danilamaimeskul <dan.maika1337@gmail.com>
2022-03-30 18:39:54 +0300

    Added validation

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,14 +1,37 @@
 const Router = require('express')
 const router = new Router()
 const userController = require('../controllers/userController')
-const authMiddleware = require('../middleware/authMiddleware')
 const {body} = require('express-validator')
+const {User} = require('../models/models')
 
 router.post('/signup',
     body('password').isLength({min: 4}),
-    body('login').isLength({min: 3}),
-    body('firstName').isLength({min: 3}),
-    body('lastName').isLength({min: 3}),
+    body('password').custom(value => {
+        if(value.search(/\d/) === -1){
+            throw new Error('Password must contain east 1 number')
+        }
+        if(value.search(/[a-zA-Z]/) === -1){
+            throw new Error('Password must contain east 1 letter')
+        }
+        return true
+    }),
+    body('repeatPassword').custom((value, {req}) => {
+        if(value !== req.body.password){
+            throw new Error('Password confirmation does not match password')
+        }
+        return true
+    }),
+    body('age').isNumeric().withMessage('Age must be a number'),
+    body('age').custom(value => {
+        if(value <= 0){
+            throw new Error(`Age must be a number, can not be a zero`)
+        }
+        return true
+    }),
+    body('login').isLength({min: 3}).withMessage('Username must contain 3 symbols or more'),
+    body('firstName').isLength({min: 3}).withMessage('First name must contain 3 symbols or more.'),
+    body('lastName').isLength({min: 3}).withMessage('Last name must contain 3 symbols or more.'),
+
     userController.signup)
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
